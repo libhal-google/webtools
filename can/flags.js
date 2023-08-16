f26f6f1cabf5fe6a42616142d9bd25200dd74bf1
Khalil Estell <kammce@google.com>
2023-08-15 21:16:31 -0700

    :tada: Start of the Web CANable interface

---
+++ can/flags.js
@@ -0,0 +1,103 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+class Flags {
+  constructor() {
+    this.model = {};
+  }
+  //sets localStorage when value is changed
+  setCache(cname, cvalue) {
+    localStorage.setItem(cname, JSON.stringify(cvalue));
+  }
+  cachedValueExists(cname) {
+    return localStorage.getItem(cname) != null ? true : false;
+  }
+  getCache(cname) {
+    let cached_data = localStorage.getItem(cname);
+    return cached_data !== "undefined" ? JSON.parse(cached_data) : null;
+  }
+  get(id) {
+    return this.model[id]["data"];
+  }
+  bind(id, update_callback, default_value) {
+    this.model[id] = {
+      data: default_value,
+      change_event: update_callback,
+    };
+  }
+  attach(id, event_type, default_value, element_callback, update_callback) {
+    let element = document.querySelector(`#${id}`);
+    // If element doesn't exist do not attempt to attach flag listeners to it
+    if (!element) {
+      console.error(`Could not find DOM element corresponding to id #${id}`);
+      return false;
+    }
+    // Assume that the element is a <select><option> combo box
+    let property = "value";
+    if (element.type === "checkbox") {
+      property = "checked";
+    }
+    // Setup event listener on element
+    element.addEventListener(event_type, () => {
+      // Get value of element and remove quotes with
+      var val = element[property];
+      // Save the value to the IDs name in LocalStorage
+      this.model[id]["data"] = val;
+      // Call their element_callback if it exists
+      if (element_callback) {
+        element_callback(val, element);
+      }
+    });
+    // Create a wrapper function that updates the elements property and calls
+    // update callback if it exists.
+    let input_updater = (value) => {
+      console.log("Input Updater!");
+      element[property] = value;
+      if (update_callback) {
+        update_callback(value, id);
+      }
+    };
+
+    this.bind(id, input_updater, default_value);
+  }
+  set(id, value) {
+    this.model[id] = value;
+    if (typeof this.model[id]["change_event"] === "function") {
+      this.model[id]["change_event"](value, id);
+    }
+  }
+  initialize() {
+    console.debug("Initializing Flags!");
+    for (var id in this.model) {
+      let cached_data = this.getCache(id);
+      if (cached_data) {
+        this.model[id]["data"] = cached_data;
+      }
+      if (this.model[id]["data"]) {
+        this.model[id]["change_event"](this.model[id]["data"], id);
+      }
+    }
+  }
+  teardown() {
+    console.debug("Saving flags to LocalStorage!");
+    for (var id in this.model) {
+      if (
+        typeof this.model[id]["data"] !== "undefined" &&
+        this.model[id]["data"] !== null
+      ) {
+        this.setCache(id, this.model[id]["data"]);
+      }
+    }
+  }
+}
---
+++ can/main.js
@@ -0,0 +1,203 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+let history_position = 0;
+let command_history = [];
+const flags = new Flags();
+const change_event = new Event("change");
+const collator = new Intl.Collator(undefined, {
+  numeric: true,
+  sensitivity: "base",
+});
+
+document.querySelector("#options-btn").addEventListener("click", () => {
+  document.querySelector("#options-dialog").showModal();
+});
+
+document
+  .querySelector("#close-options-dialog-btn")
+  .addEventListener("click", () => {
+    document.querySelector("#options-dialog").close();
+  });
+
+document.querySelector("#help-btn").addEventListener("click", () => {
+  document.querySelector("#help-dialog").showModal();
+});
+
+document
+  .querySelector("#close-help-dialog-btn")
+  .addEventListener("click", () => {
+    document.querySelector("#help-dialog").close();
+  });
+
+document.querySelector("#upload-btn").addEventListener("click", () => {
+  document.querySelector("#upload-dialog").showModal();
+});
+
+document
+  .querySelector("#close-upload-dialog-btn")
+  .addEventListener("click", () => {
+    document.querySelector("#upload-dialog").close();
+  });
+
+document.querySelector("#connect-btn").addEventListener("click", async () => {
+  device_connected ? disconnectFromDevice() : connectToDevice();
+});
+
+document.querySelector("#clear-btn").addEventListener("click", () => {
+  clearTerminal();
+});
+
+document.querySelector("#dtr-checkbox").addEventListener("click", async () => {
+  let dataTerminalReady = !!document.querySelector("#dtr-checkbox").checked;
+  await port.setSignals({ dataTerminalReady });
+});
+
+document.querySelector("#rts-checkbox").addEventListener("click", async () => {
+  let requestToSend = !!document.querySelector("#rts-checkbox").checked;
+  await port.setSignals({ requestToSend });
+});
+
+document.querySelector("#clear-history-btn").addEventListener("click", () => {
+  command_history = [];
+  history_position = 0;
+  document.querySelector("#serial-input").value = "";
+  document.querySelector("#options-dialog").close();
+});
+
+document.querySelector("#serial-input").addEventListener("keyup", (event) => {
+  const DOWN_ARROW = 38;
+  const UP_ARROW = 40;
+  const ENTER_KEY = 13;
+
+  let count_change_flag = true;
+
+  switch (event.which) {
+    case UP_ARROW:
+      if (history_position > 0) {
+        history_position--;
+      }
+      break;
+    case DOWN_ARROW:
+      if (history_position < command_history.length) {
+        history_position++;
+      }
+      break;
+    case ENTER_KEY:
+      document.querySelector("#send-btn").click();
+      break;
+    default:
+      count_change_flag = false;
+      break;
+  }
+
+  if (count_change_flag) {
+    let command = command_history[command_history.length - history_position];
+    if (command) {
+      document.querySelector("#serial-input").value = command;
+    }
+  }
+});
+
+document.querySelector("#baudrate").addEventListener("change", function () {
+  var baudrate_custom = document.querySelector("#baudrate_custom");
+
+  if (this.value === "custom") {
+    baudrate_custom.style.display = "";
+  } else {
+    baudrate_custom.style.display = "none";
+  }
+});
+
+document.querySelector("#send-btn").addEventListener("click", async () => {
+  const input = document.querySelector("#serial-input").value;
+  if (input !== command_history[command_history.length - 1]) {
+    command_history.push(input);
+  }
+  history_position = 0;
+  if (flags.get("local-echo-select")) {
+    term.writeln("\x1b[32m" + input + "\x1b[0m");
+  }
+  document.querySelector("#serial-input").value = "";
+  await writeToDevice(input);
+});
+
+document.querySelector("#serial-upload").addEventListener("click", () => {
+  let serial_file = document.querySelector("#serial-file").files;
+  if (!device_connected) {
+    alert("Please connect a device before uploading a file.");
+    return;
+  } else if (serial_file.length === 0) {
+    alert("No file selected");
+    console.debug("No file");
+    return;
+  }
+
+  let file = serial_file.item(0);
+  let reader = new FileReader();
+
+  reader.onload = async () => {
+    const writer = port.writable.getWriter();
+    await writer.write(reader.result);
+    writer.releaseLock();
+  };
+  reader.readAsArrayBuffer(file);
+});
+
+function generateCommandListHtml(command_list) {
+  if (!command_list) {
+    return "";
+  }
+
+  let html = "";
+  for (let command of command_list) {
+    html += `<option value="${command}" />`;
+  }
+  return html;
+}
+
+flags.attach("baudrate", "change", "115200");
+flags.attach("carriage-return-checkbox", "change");
+flags.attach("newline-select", "change", true);
+flags.attach("local-echo-select", "change", false);
+flags.bind(
+  "command-history",
+  (command_list) => {
+    document.querySelector("#command-history").innerHTML =
+      generateCommandListHtml(command_list);
+    console.debug("Command history updated");
+  },
+  []
+);
+
+window.addEventListener("load", () => {
+  flags.initialize();
+  var dropdown = document.querySelector("#baudrate");
+
+  var event = new Event("change", {
+    bubbles: true,
+    cancelable: true,
+  });
+
+  dropdown.dispatchEvent(event);
+});
+
+window.onbeforeunload = () => {
+  let command_history = flags.get("command-history");
+  if (command_history) {
+    flags.set("command-history", command_history.slice(0, 99));
+  }
+  flags.teardown();
+  return null;
+};
